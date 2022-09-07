0f12eb89fad6d0f81f938de21c2933cc0e446cc8
chihatw <chiha.tw@gmail.com>
2022-09-07 19:56:49 +0900

    bugfix

--- src/pages/Workout/WorkoutListPage/index.tsx
+++ src/pages/Workout/WorkoutListPage/index.tsx
@@ -32,14 +32,18 @@ const WorkoutListPage = () => {
       <div style={{ height: 48 }} />
       <div style={{ height: 16 }} />
       <div style={{ display: 'grid', rowGap: 8 }}>
-        <CustomLabel label='反応練習' />
-        {Object.values(state.workout.workouts).map((workout, index) => (
-          <WorkoutRow
-            key={index}
-            blob={state.workout.blobs[workout.id]}
-            workout={workout}
-          />
-        ))}
+        {!!Object.values(state.workout.workouts).length && (
+          <>
+            <CustomLabel label='反応練習' />
+            {Object.values(state.workout.workouts).map((workout, index) => (
+              <WorkoutRow
+                key={index}
+                blob={state.workout.blobs[workout.id]}
+                workout={workout}
+              />
+            ))}
+          </>
+        )}
         {!!Object.values(state.workingMemories).length && (
           <>
             <CustomLabel label='記憶練習' />
