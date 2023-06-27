706fdd8dc7d082c083ee7379111e03e9b300573f
chihatw <chiha.tw@gmail.com>
2023-06-28 05:51:20 +0900

    debug fontFamily

--- src/views/pages/ScorePage/commons/CorrectAnswer.tsx
+++ src/views/pages/ScorePage/commons/CorrectAnswer.tsx
@@ -1,13 +1,10 @@
 import CheckIcon from '@mui/icons-material/Check';
-import { useTheme } from '@mui/material';
 import React from 'react';
 
 const CorrectAnswer = ({ children }: { children: React.ReactNode }) => {
-  const theme = useTheme();
   return (
     <div
       style={{
-        ...(theme.typography as any).notoSerifJP,
         color: '#52a2aa',
         fontSize: 14,
         display: 'flex',
